636cf5d57b7652c8b6484c32d3bb93e029c869b6
dasbacete <dalura97@gmail.com>
2025-03-16 22:27:09 +0100

    hdltool v1 working

--- toolbox/toolibs/ghdl.py
+++ toolbox/toolibs/ghdl.py
@@ -5,13 +5,12 @@ class ghdl_handler ( handler.handler, pfile.pfile ):
 
     def __init__ ( self, pff ): 
         handler.handler.__init__(self, tool='ghdl')
-        if pff:
-            pfile.pfile.__init__( self, pfile_path=pff )
-        self.analyze_flags = "-s --std=08 --ieee=standard"
-        self.elaborate_flags = "-e"
-        self.simulate_flags = "-r"
+        pfile.pfile.__init__( self, pfile_path=pff )
+        self.analyze_flags = "--std=08 --ieee=standard"
+        self.elaborate_flags = "--std=08"
+        self.simulate_flags = ""
         
-    def analyze ( self, pfile_path=None ):
+    def analyze ( self, pfile_path=None, analyze_flags=None ):
         srcs = None
         local_pfile = None
         if pfile_path:
@@ -20,25 +19,26 @@ class ghdl_handler ( handler.handler, pfile.pfile ):
         else:
             srcs = self.list_sources()
         filelist = ' '.join(srcs)
-        flags = f"{self.analyze_flags} {filelist}"
+        flags = f"-a {self.analyze_flags} {filelist}"
         return self.launch(flags)
 
-    def elaborate ( self, top=None ):
-        top_unit = self.top
+    def elaborate ( self, top=None, elaborate_flags=None ):
+        top_unit = self.get_top()
         if top is not None:
             top_unit = top
-        flags = f"{self.elaborate_flags} {top_unit}"
+        flags = f"-e {self.elaborate_flags} {top_unit}"
         return self.launch(flags)
 
     def simulate ( self, top=None, simulate_flags=None):
-        top_unit = self.top
+        top_unit = self.get_top()
         if top is not None:
             top_unit = top
         sim_flags = ""
-        if top_unit in self.pdata:
-            if 'flags' in self.pdata[top_unit]:
-                sim_flags += f"{self.pdata[top_unit]['flags']}"
+        if self.sim:
+            if top_unit in self.sim:
+                if 'sim_flags' in self.sim[top_unit]:
+                    sim_flags += f"{self.sim[top_unit]['sim_flags']}"
         if simulate_flags is not None:
             sim_flags = f"{simulate_flags}"
-        flags = f"{self.simulate_flags} {top_unit} {sim_flags}"
+        flags = f"-r {self.simulate_flags} {top_unit} {sim_flags}"
         return self.launch(flags)
--- toolbox/toolibs/pfile.py
+++ toolbox/toolibs/pfile.py
@@ -6,37 +6,52 @@ from pathlib import Path
 class pfile:
     
     def __init__ ( self, pfile_path=None ):    
-        self.project_name = Path(pfile_path).name
-        self.pfile_path = os.path.abspath(Path(pfile_path).parent)
-        self.pdata = None
+        self.project_name = None
+        self.pfile_path   = None
+        self.top          = None
+        self.pkg          = []
+        self.rtl          = []
+        self.sim          = dict() 
         if pfile_path:
-            with open( pfile_path, 'r' ) as pff:
-                self.pdata = yaml.safe_load( pff )
-
+            ## Location
+            self.project_name = Path(pfile_path).name
+            self.pfile_path = os.path.abspath(Path(pfile_path).parent)
+            # load pfile
+            pdata = None
+            if pfile_path:
+                with open( pfile_path, 'r' ) as pff:
+                    pdata = yaml.safe_load( pff )
+            # load deps
+            if 'deps' in pdata:
+                for dep in pdata['deps']:
+                    deps_pff = pfile( self.pfile_path / Path(dep) )
+                    if deps_pff.pkg:
+                        self.pkg = self.pkg + deps_pff.pkg
+                    if deps_pff.rtl:
+                        self.rtl = self.rtl + deps_pff.rtl
+                    if deps_pff.sim:
+                        self.sim = self.sim | deps_pff.sim
+            # load top
+            if 'top' in pdata:
+                self.top = pdata['top']
+            # load current 
+            if 'pkg' in pdata['srcs']:
+                self.pkg += [ f"{self.pfile_path}/pkg/{file}" for file in pdata['srcs']['pkg'] ]
+            if 'rtl' in pdata['srcs']:
+                self.rtl += [ f"{self.pfile_path}/rtl/{file}" for file in pdata['srcs']['rtl'] ]
+            if 'sim' in pdata:
+                for k in pdata['sim']:
+                    pdata['sim'][k]['tb'] = f"{self.pfile_path}/sim/{pdata['sim'][k]['tb']}"
+                self.sim = self.sim | pdata['sim']
+    
     def list_sources( self ):
-        srcs = [] 
-        if self.pdata is not None and 'srcs' in self.pdata:
-            ## Common dependencies
-            for k in self.pdata['srcs']:
-                if k != 'pkg' and k != 'rtl':
-                    if 'pkg' in self.pdata['srcs'][k]:
-                        srcs += [ f"{self.pfile_path}/{k}/pkg/{file}" for file in self.pdata['srcs'][k]['pkg'] ]
-                    if 'rtl' in self.pdata['srcs'][k]:
-                        srcs += [ f"{self.pfile_path}/{k}/rtl/{file}" for file in self.pdata['srcs'][k]['rtl'] ]
-                    if 'sim' in self.pdata['srcs'][k]:
-                        srcs += [ f"{self.pfile_path}/{k}/sim/{file}" for file in self.pdata['srcs'][k]['sim'] ]
-            ## Main repo dependencies
-            if 'pkg' in self.pdata['srcs']:
-                srcs += [ f"{self.pfile_path}/pkg/{file}" for file in self.pdata['srcs']['pkg'] ]
-            if 'rtl' in self.pdata['srcs']:
-                srcs += [ f"{self.pfile_path}/rtl/{file}" for file in self.pdata['srcs']['rtl'] ]
-            if 'sim' in self.pdata:
-                srcs += [ f"{self.pfile_path}/sim/{test['rtl']}" for test in self.pdata['sim'] ]
+        sim_srcs = []
+        for test in self.sim:
+            sim_srcs.append(self.sim[test]['tb'])
+        return self.pkg + self.rtl + sim_srcs
 
-        return srcs
+    def get_sim_flags( self, tb ):
+        return self.sim[tb]['sim_flags']
 
-    def get_top():
-        if 'top' in self.pdata:
-            return self.pdata['top']
-        else:
-            return None
+    def get_top( self ):
+        return self.top
